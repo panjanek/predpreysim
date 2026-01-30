588d97a1f2e10ea87992881e6487ce2f3c8869d1
Maciej Siekierski <m.siekierski@insys.pl>
2026-01-30 11:48:18 +0100

    memory and speed tuned

---
+++ src/PredPreySim/Models/NN/NetworkConfig.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PredPreySim.Models.NN
+{
+    public class NetworkConfig
+    {
+        public int inputs;
+
+        public int hidden;
+
+        public int outputs;
+    }
+}
